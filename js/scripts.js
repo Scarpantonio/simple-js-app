8d933999b88bd66ad58516cdb175d60c19e56bf2
cmfarrell72 <59768789+cmfarrell72@users.noreply.github.com>
2020-01-30 18:38:47 -0600

    Update scripts.js
    Added 10 pokémon characters to repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-
+var repository = [
+  { name: 'bulbasaur', height: 7, types: 'grass, fire' },
+  { name: 'cloyster', height: 2, types: 'ice, water' },
+  { name: 'charmander', height: 2, types: 'fire' },
+  { name: 'beedrill', height: 1, types: 'bug, poison' },
+  { name: 'beedrill', height: 1, types: 'bug, poison' },
+  { name: 'gloom', height: 1, types: 'grass, poison' },
+  { name: 'hitmonlee', height: 2, types: 'fighting' },
+  { name: 'combee', height: 1, types: 'bug, flying' },
+  { name: 'tornadus', height: 2, types: 'flying' },
+  { name: 'sawk', height: 2, types: 'fighting' }
+];
