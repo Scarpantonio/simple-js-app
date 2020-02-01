3c467a00df49b552c21c3fb09bf08b0852ea3ca7
cmfarrell72 <59768789+cmfarrell72@users.noreply.github.com>
2020-02-01 17:34:07 -0600

    Update scripts.js
    created a for loop that iterates over each item in the repository. Also, used document.write() to write the Pokémon name and height on DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 var repository = [
-  { name: 'bulbasaur', height: 7, types: ['grass', 'fire'] },
-  { name: 'cloyster', height: 2, types: ['ice', 'water'] },
-  { name: 'charmander', height: 2, types: ['fire'] },
-  { name: 'beedrill', height: 1, types: ['bug', 'poison'] },
-  { name: 'beedrill', height: 1, types: ['bug', 'poison'] },
-  { name: 'gloom', height: 1, types: ['grass', 'poison'] },
-  { name: 'hitmonlee', height: 2, types: ['fighting'] },
-  { name: 'combee', height: 1, types: ['bug', 'flying'] },
-  { name: 'tornadus', height: 2, types: ['flying'] },
-  { name: 'sawk', height: 2, types: ['fighting'] }
+  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'fire'] },
+  { name: 'Cloyster', height: 1.5, types: ['ice', 'water'] },
+  { name: 'Charmander', height: 0.6, types: ['fire'] },
+  { name: 'Beedrill', height: 1, types: ['bug', 'poison'] },
+  { name: 'Gloom', height: 0.8, types: ['grass', 'poison'] },
+  { name: 'Gitmonlee', height: 1.5, types: ['fighting'] },
+  { name: 'Combee', height: 0.3, types: ['bug', 'flying'] },
+  { name: 'Tornadus', height: 1.5, types: ['flying'] },
+  { name: 'Sawk', height: 1.4, types: ['fighting'] },
+  { name: 'Onix', height: 8.8, types: ['rock', 'ground'] }
 ];
+
+for (let i = 0; i < repository.length; i++) {
+  let pokemon = repository[i];
+  document.write(
+    pokemon.name +
+      ' ' +
+      '(' +
+      'height:' +
+      ' ' +
+      pokemon.height +
+      ')' +
+      '<br>' +
+      '<br>'
+  );
+  if (pokemon.height > 5) {
+    document.write("Wow, that's big!");
+  }
+}
