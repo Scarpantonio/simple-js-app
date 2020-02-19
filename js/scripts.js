2ba6dedff3038dd448dd50d7adfd2e86e0266045
cmfarrell72 <59768789+cmfarrell72@users.noreply.github.com>
2020-02-19 06:30:59 -0600

    script.js
    Edit the showDetails() to show a modal with Pokémon name, height, and an image

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,7 @@ var pokemonRepository = (function() {
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function() {
       console.log(item);
+      showModal(item);
     });
   }
 
@@ -41,6 +42,7 @@ var pokemonRepository = (function() {
             detailsUrl: item.url
           };
           add(pokemon);
+          console.log(pokemon);
         });
       })
       .catch(function(e) {
@@ -58,18 +60,109 @@ var pokemonRepository = (function() {
         // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = Object.keys(details.types);
+        // loop for each of the pokemon types
+        item.types = [];
+        for (var i = 0; i < details.types.length; i++) {
+          item.types.push(details.types[i].type.name);
+        }
+
+        //loop to get the abilities of a selected pokemon
+        item.abilities = [];
+        for (var i = 0; i < details.abilities.length; i++) {
+          item.abilities.push(details.abilities[i].ability.name);
+          // item.abilities.push('slot: ' + details.abilities[i].slot);
+          // item.abilities.push('is_hidden: ' + details.abilities[i].is_hidden);
+        }
+
+        item.weight = details.weight;
       })
       .catch(function(e) {
         console.error(e);
       });
   }
+  // Show modal content
+  function showModal(item) {
+    var $modalContainer = document.querySelector('#modal-container');
+    // Clear existing modal content
+    $modalContainer.innerHTML = '';
+    // Creating div element in DOM
+    var modal = document.createElement('div');
+    // adding class to div DOM element
+    modal.classList.add('modal');
+    // create closing button in modal content
+    var closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    // Add event listener to close modal when botton is clicked
+    closeButtonElement.addEventListener('click', hideModal);
+    // Create element for name in modal content
+    var nameElement = document.createElement('h1');
+    nameElement.innerText = item.name;
+    // Create img in modal content
+    var imageElement = document.createElement('img');
+    imageElement.classList.add('modal-img');
+    imageElement.setAttribute('src', item.imageUrl);
+    // Create element for height in modal content
+    var heightElement = document.createElement('p');
+    heightElement.innerText = 'height : ' + item.height + 'm';
+    // create element for weight in modal content
+    var weightElement = document.createElement('p');
+    weightElement.innerText = 'weight : ' + item.weight + 'kg';
+    // Create element for type in modal content
+    var typesElement = document.createElement('p');
+    typesElement.innerText = 'types : ' + item.types;
+    // Create element for abilities in modal content
+    var abilitiesElement = document.createElement('p');
+    abilitiesElement.innerText = 'abilities : ' + item.abilities;
+
+    // Append modal content to webpage
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(nameElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(weightElement);
+    modal.appendChild(typesElement);
+    modal.appendChild(abilitiesElement);
+    $modalContainer.appendChild(modal);
+
+    // Add class to show modal
+    $modalContainer.classList.add('is-visible');
+  }
+
+  // hides modal when close button is clicked
+  function hideModal() {
+    var $modalContainer = document.querySelector('#modal-container');
+    $modalContainer.classList.remove('is-visible');
+  }
+
+  // Hides model when ESC is clicked
+  window.addEventListener('keydown', e => {
+    var $modalContainer = document.querySelector('#modal-container');
+    if (
+      e.key === 'Escape' &&
+      $modalContainer.classList.contains('is-visible')
+    ) {
+      hideModal();
+    }
+  });
+
+  // Hides modal if clicked outside of it
+  var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.addEventListener('click', e => {
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
   };
 })();
 
